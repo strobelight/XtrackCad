17a207dad205dfcec3e53bae4158aa8277072488
Ken Shaffer <8398167+strobelight@users.noreply.github.com>
2022-12-31 17:02:46 -0500

    flexible versions; show cmake version

--- debian/BuildXtrackCad.sh
+++ debian/BuildXtrackCad.sh
@@ -14,38 +14,51 @@ echo "#############################################################"
 echo "#    G E T   X T R A C K C A D   S O U R C E"
 echo "#############################################################"
 
-XtrackPath="$HOME/XtrackCAD/V5.2.2"
-XtrackVer="$(basename $XtrackPath)"
+#XtrackVer="V5.2.2"
+XtrackVer="V5.2.2a"
+#XtrackVer="default"
+
+XtrackPath="$HOME/XtrackCAD/$XtrackVer"
+XtrackSrc="$HOME/XtrackCAD/src"
+XtrackBuildDir="$XtrackPath/build-dbg"
 XtrackInstallPrefix="$XtrackPath/install-dbg"
+
+cmake --version
+sleep 2
 
 cd $HOME
 rm -rf $XtrackPath
-mkdir -p $XtrackPath/build-dbg
-cd $XtrackPath || exit
-hg clone http://hg.code.sf.net/p/xtrkcad-fork/xtrkcad src
-cd src
+mkdir -p $XtrackBuildDir
+
+mkdir -p $XtrackSrc
+hg clone http://hg.code.sf.net/p/xtrkcad-fork/xtrkcad $XtrackSrc
+cd $XtrackSrc
 echo "Update for version $XtrackVer"
 hg update $XtrackVer
-cd ../build-dbg
+cd $XtrackBuildDir
 
 # defining location of cmocka library resulted in compile errors of unit tests
-#cmake -DCMAKE_BUILD_TYPE=Debug -DCMOCKA_LIBRARY=/usr/lib/aarch64-linux-gnu/libcmocka.so -DCMAKE_INSTALL_PREFIX=$XtrackInstallPrefix -DCMAKE_C_FLAGS=-Wpointer-sign -DXTRKCAD_USE_GETTEXT=ON ../src
+#cmake -DCMAKE_BUILD_TYPE=Debug -DCMOCKA_LIBRARY=/usr/lib/aarch64-linux-gnu/libcmocka.so -DCMAKE_INSTALL_PREFIX=$XtrackInstallPrefix -DCMAKE_C_FLAGS=-Wpointer-sign -DXTRKCAD_USE_GETTEXT=ON $XtrackSrc
 
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$XtrackInstallPrefix -DCMAKE_C_FLAGS=-Wpointer-sign -DXTRKCAD_USE_GETTEXT=ON ../src
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$XtrackInstallPrefix -DCMAKE_C_FLAGS=-Wpointer-sign -DXTRKCAD_USE_GETTEXT=ON $XtrackSrc
 
 make || exit
 make install || exit
 
 if [ -d $HOME/.local/share/applications ]; then
     mkdir -p $HOME/.local/bin
-    cat <<-EOF > $HOME/.local/bin/startXtrkCad
+    cd $HOME/.local/bin
+    cat <<-EOF > startXtrkCad_$XtrackVer
 	#!/bin/bash
 	unset LD_LIBRARY_PATH
+	unset XTRKCADLIB
+	# comment out if help works relative to current directory
 	export XTRKCADLIB=$XtrackInstallPrefix/share/xtrkcad
 	cd $XtrackInstallPrefix/bin
 	exec $XtrackInstallPrefix/bin/xtrkcad
 	EOF
-    chmod +x $HOME/.local/bin/startXtrkCad
+    chmod +x startXtrkCad_$XtrackVer
+    ln -s -f startXtrkCad_$XtrackVer startXtrkCad
     cat <<-EOF > $HOME/.local/share/applications/xtrkcad.desktop
 	[Desktop Entry]
 	Name=XTrackCAD
